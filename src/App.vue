c9bce5bc7bd84b11d3bd93849475abba0a9c32e6
Agustin Karkocki <agustin.karkocki@gmail.com>
2022-03-13 18:53:12 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+  @import "@/styles/index.scss";
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/styles/_variables.scss";
+        `
+      }
+    }
+  }
+};
